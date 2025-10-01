833cd09b507c1f83010e4453e70e2e33e69ed436
g00485105 <g00485105@atu.ie>
2025-10-01 09:59:13 +0000

    changes

--- BigJava.java
+++ BigJava.java
@@ -1,7 +1,11 @@
 public class BigJava {
     public static void main (String[] args) {
-        System.out.println("\t***\n\t***\n\t***\n***\t***\n ***\t***\n  ***   **\n     *** "); System.out.println("\t****\n       ******\n      ***  ***\n     **********\n    ************\n   ***\t      ***\n  ***\t       ***");
-        
-
+        System.out.println("        ***         ****      ***            ***      ****"); 
+        System.out.println("        ***        ******      ***          ***      ******");       
+        System.out.println("        ***       ***  ***      ***        ***      ***  ***");
+        System.out.println("***     ***      **********      ***      ***      **********");
+        System.out.println(" ***    ***     ************      ***    ***      ************");
+        System.out.println("  ***   **     ***        ***      ***  **       ***        ***");
+        System.out.println("     ***      ***          ***       ***        ***          ***");
     }
 }
---
+++ PetrolReceipt.java
@@ -0,0 +1,16 @@
+public class PetrolReceipt {
+    public static void main (String[] args) {
+        System.out.println("+-------------------------+"); 
+        System.out.println("|                         |");       
+        System.out.println("|       Local Garage      |");
+        System.out.println("|                         |");
+        System.out.println("| 01-OCT-2025  10:10PM    |");
+        System.out.println("|                         |"); 
+        System.out.println("| Litres:       44.00     |");       
+        System.out.println("| Price/litre:   1.71     |");
+        System.out.println("|                         |");
+        System.out.println("| Fuel total:    75.24    |");
+        System.out.println("|                         |");       
+        System.out.println("+-------------------------+");
+     }
+}
