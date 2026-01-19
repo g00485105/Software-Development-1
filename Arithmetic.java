c716bc1013506b2211bc5a867b7e835e50180a0e
g00485105 <g00485105@atu.ie>
2026-01-19 13:53:30 +0000

    lab2

---
+++ Arithmetic.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Arithmetic {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int first, second, third, result;
+
+        System.out.print("Enter first number: ");
+        first = input.nextInt();
+        System.out.print("Enter second number: ");
+        second = input.nextInt();
+        
+        result = first + second;
+        System.out.println(first + " + " + second + " = " + result);
+
+        third = 10;
+
+        System.out.print(result);
+        //result = result + third;
+        result += third;
+
+        System.out.println(" + " + third + " = " + result);
+
+        int number = 0;
+        
+        System.out.println(number);
+        System.out.println(number++);
+        System.out.println(number);
+        System.out.println(++number);
+        number--;
+        System.out.println(number);
+        System.out.println(--number);
+
+        number += 4;
+        System.out.println(number);   
+        number -= 2;
+        System.out.println(number);
+        number += 3;
+        System.out.println(number);
+        number /= 3;
+        System.out.println(number);
+        number -= 2;
+        System.out.println(number);
+             
+        input.close();
+
+    }
+}
