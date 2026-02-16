abee76fa885e814175b9a2ab2bfebf62a5b5d916
g00485105 <g00485105@atu.ie>
2026-02-16 11:48:45 +0000

    s2w5

---
+++ Even.java
@@ -0,0 +1,8 @@
+public class Even {
+    public static void main(String[] args) {
+        
+        for (int counter = 2; counter <= 10; counter += 2) {
+            System.out.println(counter);
+        }
+    }
+}
